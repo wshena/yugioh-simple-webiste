246bd8bc695018f42eb02317c420f33adb2f6c4d
wshena <81917362+wshena@users.noreply.github.com>
2023-01-08 21:15:06 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+$('#search-button').on('click', function () {
+    $.ajax({
+        url: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?',
+        type: 'get',
+        dataType: 'json',
+        data: {
+            'name' : $('#search-input').val()
+        },
+        success: function(result) {
+            let cards = result.data
+            console.log(cards)
+            $.each(cards, function(i, data) {
+                $('#yugioh-list').append(`
+                    <div class="col-md-4">
+                        <div class="card" style="width: 18rem; padding-top: 10px; margin-bottom: 10px">
+                            <img src="`+data.card_images[0].image_url+`" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">`+data.name+`</h5>
+                                <p class="mb-0">Type : `+data.type+`</p>
+                                <p>Race : `+data.race+`</p>
+                            </div>
+                        </div>
+                    </div>
+                    `)
+            })
+        }
+    })
+})
