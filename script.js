0b035592d6c6e88f0e5ec75039aac30266d74a87
wshena <81917362+wshena@users.noreply.github.com>
2023-01-09 20:25:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-$('#search-button').on('click', function () {
+function SeacrhCard() {
+    $('#yugioh-list').html('');
+
     $.ajax({
         url: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?',
         type: 'get',
@@ -12,17 +14,28 @@ $('#search-button').on('click', function () {
             $.each(cards, function(i, data) {
                 $('#yugioh-list').append(`
                     <div class="col-md-4">
-                        <div class="card" style="width: 18rem; padding-top: 10px; margin-bottom: 10px">
+                        <div class="card" style="width: 18rem; margin-bottom: 10px">
                             <img src="`+data.card_images[0].image_url+`" class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title">`+data.name+`</h5>
                                 <p class="mb-0">Type : `+data.type+`</p>
                                 <p>Race : `+data.race+`</p>
+                                <a href="#"> Detail </a>
                             </div>
                         </div>
                     </div>
                     `)
-            })
+            })            
         }
     })
+}
+
+$('#search-button').on('click', function () {
+    SeacrhCard();
+})
+
+$('#search-input').on('keyup', function(e) {
+    if (e.keyCode == 13) {
+        SeacrhCard();
+    }
 })
